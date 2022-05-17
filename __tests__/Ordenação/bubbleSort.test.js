e8659ebf3b0c9c84083876ab572fbdf588c1c88e
GustavoMaciel <gm88605363@gmail.com>
2022-05-17 08:40:10 -0300

    atualização geral

--- __tests__/Ordenação/bubbleSort.test.js
+++ __tests__/Ordenação/bubbleSort.test.js
@@ -2,9 +2,10 @@ import bubbleSort from "../../src/Ordenação/bubbleSort";
 
 
 test("Ordenação basica",
-    () => {
+     () => {
         let dadosNaoOrdenados = [5,7,8,9,4,10]
         let sequenciaOrdenada= [4,5,7,8,9,10]
+        
         expect(bubbleSort(dadosNaoOrdenados)).toStrictEqual(sequenciaOrdenada);
     }
 )
--- src/Ordenação/bubbleSort.js
+++ src/Ordenação/bubbleSort.js
@@ -3,7 +3,13 @@ function bubbleSort(dadosNaoOrdenados){
     let DadosOrdenados;
 
     for(i=0; i < dadosNaoOrdenados.length; i++){
+
+        // O I acaba tendo que rodar todo o array
+        
         for(j=0; j <= dadosNaoOrdenados.length - 1 - i; j++){
+            
+            // O J para no penultimo já que ele já consegue olhar para o ultimo;
+
             if(dadosNaoOrdenados[j] > dadosNaoOrdenados[j++]){
 
                 aux = dadosNaoOrdenados[j]
